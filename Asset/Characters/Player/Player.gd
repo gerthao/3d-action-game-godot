740e8d4ed75e791064214c6beaa87d23071c4b84
Ger Thao <g.thao19@gmail.com>
2023-10-19 20:16:43 -0500

    add project to github, add player movment, animations when moving, add swords to player model, add coins with spinning animation.

---
+++ Asset/Characters/Player/Player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody3D
+
+@onready var visual: Node3D = $Visual
+@onready var animation_player: AnimationPlayer = $Visual/AnimationPlayer
+@onready var footstep_vfx: GPUParticles3D = $Visual/VFX/Footstep_GPUParticles3D
+
+const SPEED = 5.0
+const ANGULAR_SPEED = 7.0
+
+var coins: int
+
+func _physics_process(delta: float) -> void:
+	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down").rotated(-45)
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		animation_player.play("LittleAdventurerAndie_Run")
+		footstep_vfx.emitting = true
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		animation_player.play("LittleAdventurerAndie_Idel")
+		footstep_vfx.emitting = false
+		
+	if velocity.length() > 0.2:
+		var look_dir = Vector2(velocity.z, velocity.x)
+		visual.rotation.y = lerp_angle(visual.rotation.y, look_dir.angle(), ANGULAR_SPEED * delta)
+
+	move_and_slide()
+
+
+func add_coin(value: int) -> void:
+	coins += value
+	print(coins)
---
+++ Asset/GameObjects/CollectableCoin/CollectableCoin.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@onready var visual: Node3D = $Visual
+@onready var pickup_vfx: GPUParticles3D = $VFXNode
+@onready var animation_player: AnimationPlayer = $Visual/AnimationPlayer
+
+const COIN_VALUE = 1
+var rotation_speed = 1.0
+
+func _process(delta: float) -> void:
+	visual.rotate_y(rotation_speed * delta)
+	
+	if !visual.visible && !pickup_vfx.emitting:
+		queue_free() 
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		pickup_vfx.emitting = true
+		animation_player.play("collected")
+		rotation_speed = 10.0
+		body.add_coin(COIN_VALUE)
