f133aafff7420946d939389b3203de5cde0d23c4
Ger Thao <g.thao19@gmail.com>
2023-12-03 23:00:26 -0600

    update to 4.2, add state machine for player states

--- Asset/Characters/Enemy/Enemy.gd
+++ Asset/Characters/Enemy/Enemy.gd
@@ -2,15 +2,16 @@ class_name Enemy
 extends CharacterBody3D
 
 
-const SPEED = 0.5
+const SPEED = 2.0
 
-@onready var player: Player
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var visual: Node3D = $Visual
 @onready var animation_player: AnimationPlayer = $Visual/AnimationPlayer
+@onready var player: Player
 
-var direction: Vector3
 var stop_distance: float = 2.2
+var direction: Vector3
+
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -19,8 +20,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	navigation_agent.target_position = player.global_position
 	
-	direction = navigation_agent.get_next_path_position() - global_position
-	direction.normalized()
+	direction = (navigation_agent.get_next_path_position() - global_position).normalized()
 
 	if is_close_to_player():
 		animation_player.play("NPC_01_IDLE")
---
+++ Asset/Scripts/State/BaseState.gd
@@ -0,0 +1,26 @@
+class_name BaseState extends Node
+
+var state_machine: StateMachine
+var character: CharacterBody2D
+var animation_player: AnimationPlayer
+
+@export var animation_name: String = ''
+
+
+func enter() -> void:
+	print('entering state: ', name)
+	animation_player.play(animation_name)
+	pass
+
+
+func exit() -> void:
+	print('exiting state: ', name)
+	pass
+	
+	
+func state_update(delta: float) -> void:
+	pass
+	
+	
+func show_info() -> void:
+	print(name, '/', character, '/', state_machine)
